6d57aba257b17ea6aa82cd50416b7c45885ae968
u.thiagarajan <u.thiagarajan@accenture.com>
2025-08-20 12:26:37 +0530

    Amazon-Q

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CodeComponent } from './components/code/code.component';
+import { AiReviewComponent } from './components/ai-review/ai-review.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CodeComponent,
+    AiReviewComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/ai-review/ai-review.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-review',
+  imports: [],
+  templateUrl: './ai-review.component.html',
+  styleUrl: './ai-review.component.scss'
+})
+export class AiReviewComponent {
+
+}
